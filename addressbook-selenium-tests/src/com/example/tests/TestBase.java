4fbbca8e02fadb259cd27011f10a2c11dffaf9db
YuliyaS <sy81@yandex.ru>
2014-05-28 09:49:52 +0400

    Домашнее задание 3 (версия 4)

--- addressbook-selenium-tests/src/com/example/tests/TestBase.java
+++ addressbook-selenium-tests/src/com/example/tests/TestBase.java
@@ -7,7 +7,7 @@ import com.example.fw.ApplicationManager;
 
 public class TestBase {
 
-	protected ApplicationManager app;
+	public static ApplicationManager app;
 
 	@BeforeTest
 	public void setUp() throws Exception {
