5db8a25a75838d6d4b0ee4ed5db6b66304641200
YuliyaS <sy81@yandex.ru>
2014-06-05 01:02:30 +0400

    Домашнее задание 6 (заготовка)

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,16 +1,19 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
 import java.util.Collections;
 import java.util.List;
-
-import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
 
-	@Test
-	public void testNonEmptyGroupCreation() throws Exception {
+	
+
+	@Test(dataProvider = "randomValidGroupGenerator")
+	public void testGroupCreationWithValidData(GroupData group)
+			throws Exception {
 		app.getNavigationHelper().openMainPage();
 		app.getNavigationHelper().gotoGroupsPage();
 
@@ -19,10 +22,6 @@ public class GroupCreationTests extends TestBase {
 
 		// actions
 		app.getGroupHelper().initGroupCreation();
-		GroupData group = new GroupData();
-		group.name = "group name 1";
-		group.header = "header 1";
-		group.footer = "footer 1";
 		app.getGroupHelper().fillGroupForm(group);
 		app.getGroupHelper().submitGroupCreation();
 		app.getGroupHelper().returnToGroupsPage();
@@ -37,27 +36,4 @@ public class GroupCreationTests extends TestBase {
 
 	}
 
-	@Test
-	public void testEmptyGroupCreation() throws Exception {
-		app.getNavigationHelper().openMainPage();
-		app.getNavigationHelper().gotoGroupsPage();
-
-		// save old state
-		List<GroupData> oldList = app.getGroupHelper().getGroups();
-
-		// actions
-		GroupData group = new GroupData("", "", "");
-		app.getGroupHelper().initGroupCreation();
-		app.getGroupHelper().fillGroupForm(group);
-		app.getGroupHelper().submitGroupCreation();
-		app.getGroupHelper().returnToGroupsPage();
-
-		// save new state
-		List<GroupData> newList = app.getGroupHelper().getGroups();
-
-		// compare states
-		oldList.add(group);
-		Collections.sort(oldList);
-		assertEquals(newList, oldList);
-	}
 }
