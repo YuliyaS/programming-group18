cad16bd1fa25a5c53a4f77f2ba214a25bd8a6441
YuliyaS <sy81@yandex.ru>
2014-07-10 14:24:50 +0400

    Домашнее задание 14 (работа над ошибками)

--- addressbook-selenium-tests/runTests.bat
+++ addressbook-selenium-tests/runTests.bat
@@ -1,1 +1,9 @@
-java -cp bin;c:\Tools\xstream-1.4.7\lib\xstream-1.4.7.jar;c:\Tools\xstream-1.4.7\lib\xstream\xpp3_min-1.1.4c.jar;c:\Tools\xstream-1.4.7\lib\xstream\xmlpull-1.1.3.1.jar;c:\Tools\selenium-2.41.0\selenium-server-standalone-2.41.0.jar -DconfigFile=firefox.properties org.testng.TestNG testng-customsuite.xml
+set propertiesFile=firefox.properties
+set configFile=testng-customsuite.xml
+
+set lib1=c:\Tools\xstream-1.4.7\lib\xstream-1.4.7.jar
+set lib2=c:\Tools\xstream-1.4.7\lib\xstream\xpp3_min-1.1.4c.jar
+set lib3=c:\Tools\xstream-1.4.7\lib\xstream\xmlpull-1.1.3.1.jar
+set libSelenium=c:\Tools\selenium-2.41.0\selenium-server-standalone-2.41.0.jar
+
+java -cp bin;%lib1%;%lib2%;%lib3%;%libSelenium% -DconfigFile=%propertiesFile% org.testng.TestNG %configFile%
